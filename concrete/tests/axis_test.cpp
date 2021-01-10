89709025565f726dedf8bf03eca83086e3466d79
博奕 黄 <1853308@tongji.edu.cn>
2021-01-10 21:34:33 +0800

    Update axis_test.cpp

--- concrete/tests/axis_test.cpp
+++ concrete/tests/axis_test.cpp
@@ -6,7 +6,7 @@ int main() {
   using namespace structure::fundamental;
   concrete c("2.51e4"_num, "30.8"_num, "1.97"_num);
   rebar s("2.05e5"_num, 376_num, {}, 681_num);
-  single_rebar_rectangle_section example(152_num, 152_num, 284_num, c, s);
+  longitudinal_rebar_rectangle_section example(152_num, 152_num, 284_num, c, s);
   cout << "Ntcr = " << example.Ntcr() << endl;
   cout << "Ntu = " << example.Ntu() << endl;
 }

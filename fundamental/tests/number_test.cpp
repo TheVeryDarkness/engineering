38221e207c91244008a4c7ee6c26192a763e76a6
博奕 黄 <1853308@tongji.edu.cn>
2021-01-10 11:11:55 +0800

    adding functions and tests

--- fundamental/tests/number_test.cpp
+++ fundamental/tests/number_test.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 int main() noexcept {
   using structure::fundamental::unsigned_number;
-  unsigned_number<> a(10, 3), b(10, 3);
+  unsigned_number a(10, 3), b(10, 3);
   using std::cout, std::endl;
   cout << "a = " << a.to_string() << "\nb = " << b.to_string() << endl;
   a *= b;

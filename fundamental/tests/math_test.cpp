612556261e572c29cb7266db4e39565a1d0c186d
博奕 黄 <1853308@tongji.edu.cn>
2021-01-23 21:09:32 +0800

    add a test

---
+++ fundamental/tests/math_test.cpp
@@ -0,0 +1,11 @@
+/*
+ * UTF-8 encoding
+ */
+#include "../math.hpp"
+#include <iostream>
+int main() {
+  using std::cout, std::endl;
+  using structure::fundamental::pi, structure::fundamental::pi_4;
+  cout << "pi = " << pi << endl;
+  cout << "pi / 4 = " << pi_4 << endl;
+}

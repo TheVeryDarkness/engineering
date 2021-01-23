e509a203163c9869d90763cf22523cd9a3c38725
博奕 黄 <1853308@tongji.edu.cn>
2021-01-24 00:25:14 +0800

    Update math_test.cpp

--- fundamental/tests/math_test.cpp
+++ fundamental/tests/math_test.cpp
@@ -8,5 +8,6 @@ int main() {
   using namespace structure::fundamental;
   cout << "pi = " << pi << endl;
   cout << "pi / 4 = " << pi_4 << endl;
-  cout << pow(pi, 2) << endl;
+  cout << "pi ^ 2 = " << pow(pi, 2) << endl;
+  cout << "5.10 ^ 4 = " << pow(5.10_num, 4) << endl;
 }

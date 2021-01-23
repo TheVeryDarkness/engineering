ecf1c8a9d4f3b26f8a8419dbd2d1f754984147c4
博奕 黄 <1853308@tongji.edu.cn>
2021-01-23 21:51:01 +0800

    Create calculus.hpp

---
+++ fundamental/calculus.hpp
@@ -0,0 +1,27 @@
+/*
+ *UTF-8 encoding
+ */
+#pragma once
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+namespace structure {
+namespace fundamental {
+using name_t = std::string;
+struct _module {
+  std::map<name_t, nullptr_t> functions;
+};
+static inline std::set<_module> being_used;
+class node {
+  name_t name;
+  std::vector<node> parameters;
+};
+class item {
+  std::set<node> product;
+};
+class expression {
+  std::set<item> sum;
+};
+} // namespace fundamental
+} // namespace structure

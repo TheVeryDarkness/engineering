41daf8ea95919924d008b4d72299b28178f5f1dd
博奕 黄 <1853308@tongji.edu.cn>
2021-01-24 20:48:35 +0800

    Update math_test.cpp

--- fundamental/tests/math_test.cpp
+++ fundamental/tests/math_test.cpp
@@ -3,6 +3,12 @@
  */
 #include "../math.hpp"
 #include <iostream>
+#define POW_TEST_INT(b, e)                                                                                             \
+  cout << #b "^" #e " = " << pow(b##_num, e) << endl                                                                   \
+       << #b "^" #e " = " << pow(b##_num, e##_num) << endl                                                             \
+       << #b "^" #e " = " << pow(b, e) << endl
+#define POW_TEST_DEC(b, e)                                                                                             \
+  cout << #b "^" #e " = " << pow(b##_num, e##_num) << endl << #b "^" #e " = " << pow(b, e) << endl
 int main() {
   using std::cout, std::endl;
   using namespace engineering::fundamental;
@@ -10,19 +16,18 @@ int main() {
   cout << "pi / 4 = " << pi_4 << endl;
   cout << "pi ^ 2 = " << pow(pi, 2) << endl;
 
-  cout << "5 ^ 4 = " << pow(5_num, 4) << endl;
-  cout << "5 ^ 4 = " << pow(5_num, 4_num) << endl;
-  cout << "5 ^ 4 = " << pow(5, 4) << endl;
+  POW_TEST_INT(0.3, 4);
+  POW_TEST_INT(0.30, 4);
+  POW_TEST_INT(0.30, 6);
+  POW_TEST_INT(0.9, 64);
+  POW_TEST_INT(0.90, 64);
+  POW_TEST_INT(0.900, 64);
+  POW_TEST_INT(0.9000, 64);
+  POW_TEST_INT(5, 4);
+  POW_TEST_INT(5.0, 4);
+  POW_TEST_INT(5.10, 4);
+  POW_TEST_INT(5.134, 4);
 
-  cout << "5.0 ^ 4 = " << pow(5.0_num, 4) << endl;
-  cout << "5.0 ^ 4 = " << pow(5.0_num, 4_num) << endl;
-  cout << "5.0 ^ 4 = " << pow(5.0, 4) << endl;
-
-  cout << "5.10 ^ 4 = " << pow(5.10_num, 4) << endl;
-  cout << "5.10 ^ 4 = " << pow(5.10_num, 4_num) << endl;
-  cout << "5.10 ^ 4 = " << pow(5.10, 4) << endl;
-
-  cout << "5.134 ^ 4 = " << pow(5.134_num, 4) << endl;
-  cout << "5.134 ^ 4 = " << pow(5.134_num, 4_num) << endl;
-  cout << "5.134 ^ 4 = " << pow(5.134, 4) << endl;
+  POW_TEST_DEC(0.134, 0.5);
+  POW_TEST_DEC(0.134, 2.5);
 }

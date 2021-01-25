97254aa8683ba313b7bd29cb3bb9201d486f3720
博奕 黄 <1853308@tongji.edu.cn>
2021-01-25 15:00:51 +0800

    Update math.hpp

--- fundamental/math.hpp
+++ fundamental/math.hpp
@@ -8,6 +8,4 @@ namespace engineering {
 constexpr static inline auto str_pi = "3.141592653";
 constexpr static inline auto pi = fundamental::unsigned_number::to_number(str_pi);
 constexpr static inline auto pi_4 = pi / fundamental::unsigned_number::exact(4);
-namespace fundamental {
-} // namespace fundamental
 } // namespace engineering

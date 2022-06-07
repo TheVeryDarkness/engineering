8a5d0227fe48e16a5db9a00bea68d8f998f13ed6
博奕 黄 <1853308@tongji.edu.cn>
2022-06-08 00:33:53 +0800

    大致完成加快节拍流水施工

--- engineering/construction/test/charts.py
+++ engineering/construction/test/charts.py
@@ -1,6 +1,4 @@
 import pathlib
-import os
-import sys
 
 sd = pathlib.Path(__file__)
 p = sd.parent
@@ -8,15 +6,15 @@ from .. import chart
 
 if __name__ == "__main__":
     with open(str(p / "mooc-1.svg"), "w", encoding="UTF-8") as f:
-        chart.fast_pipeline(4, [], [4, 10, 12], "d", f, False)
+        chart.accelerated_beat_flow(4, [], [4, 10, 12], "d", f, False)
     with open(str(p / "mooc-1-v.svg"), "w", encoding="UTF-8") as f:
-        chart.fast_pipeline(4, [], [4, 10, 12], "d", f, True)
+        chart.accelerated_beat_flow(4, [], [4, 10, 12], "d", f, True)
 
     with open(str(p / "textbook-11-1.svg"), "w", encoding="UTF-8") as f:
-        chart.fast_pipeline(
+        chart.accelerated_beat_flow(
             4, ["基础工程", "结构安装", "室内装修", "室外工程"], [5, 10, 10, 5], "周", f, False
         )
     with open(str(p / "textbook-11-1-v.svg"), "w", encoding="UTF-8") as f:
-        chart.fast_pipeline(
+        chart.accelerated_beat_flow(
             4, ["基础工程", "结构安装", "室内装修", "室外工程"], [5, 10, 10, 5], "周", f, True
         )
